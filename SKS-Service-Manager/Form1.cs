242ae111b2fd38961105c297362f4830dd096f4f
Dominik <dominikciesielski4@gmail.com>
2024-01-21 19:59:05 +0100

    a

--- SKS-Service-Manager/Form1.cs
+++ SKS-Service-Manager/Form1.cs
@@ -17,7 +17,7 @@ namespace SKS_Service_Manager
         private DataBase database;
 
         private string versionUrl = "https://raw.githubusercontent.com/domcie99/SKS-Service-Manager/master/SKS-Service-Manager/version.txt";
-        private string updateUrl = "https://github.com/domcie99/SKS-Service-Manager/releases/download/v{0}/SKS-Service-Manager.msi";
+        private string updateUrl = "https://github.com/domcie99/SKS-Service-Manager/blob/master/SKS-Service-Manager-Installer/SKS-Service-Manager.msi";
         private string localVersion = "1.0.1"; // Wersja Twojej aplikacji
 
         
